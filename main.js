9d86b5d24f8330541c0984ec3cbe07ecf59fbcd7
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2021-01-22 11:37:10 -0600

    No commit message

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 var state = new State(document.location.search);
 
+function addCard(column) {
+
+    var card = document.createElement("div"),
+        textarea = document.createElement("textarea"),
+        cardActions = document.createElement("div"),
+        deleteButton = document.createElement("button"),
+        detailsButton = document.createElement("button");
+
+    card.classList.add("card");
+    cardActions.classList.add("card-actions");
+    deleteButton.classList.add("delete", "card-action");
+    detailsButton.classList.add("open-details", "card-action");
+    
+    deleteButton.innerHTML = "remove";
+    detailsButton.innerHTML = "details";
+
+    cardActions.append(deleteButton, detailsButton);
+    card.append(textarea, cardActions);
+
+    document.querySelector(`#${column} .cards`).append(card);
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log(state.getAll("positive"));
     console.log(state.getAll("negative"));
