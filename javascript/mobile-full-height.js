276a9781687f21ea12a0e32ed70160e2e6d26c06
Jeremy Chandler <goldenmeso@gmail.com>
2021-02-14 14:48:39 -0600

    mobile fix

--- javascript/mobile-full-height.js
+++ javascript/mobile-full-height.js
@@ -1,11 +1,13 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.addEventListener("DOMContentLoaded", () => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    var vh = window.innerHeight;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 // We listen to the resize event
 window.addEventListener('resize', () => {
   // We execute the same script as before
-  let vh = window.innerHeight;
+  var vh = window.innerHeight;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
