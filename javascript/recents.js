1dd467509da803530fced443f27bdfc7e209d79f
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2021-02-15 15:47:57 -0600

    No commit message

--- javascript/recents.js
+++ javascript/recents.js
@@ -2,16 +2,23 @@ document.addEventListener("DOMContentLoaded", async function() {
 
     var recentsButton = document.getElementById("butRecents");
 
-    var filenames = await keys();
-    filenames.reverse().forEach(filename => {
-        var button = document.createElement("div");
-        button.innerHTML = filename; 
+    recentsButton.addEventListener("mouseenter", async function() {
+        var filenames = await keys();
 
-        button.addEventListener("click", async function() {
-            var handle = await get(this.innerHTML);
-            openFile(handle);
+        var recentsListElement = recentsButton.querySelector("div");
+        recentsListElement.innerHTML = "";
+    
+        filenames.reverse().forEach(filename => {
+            var button = document.createElement("div");
+            button.innerHTML = filename; 
+    
+            button.addEventListener("click", async function() {
+                var handle = await get(this.innerHTML);
+                openFile(handle);
+            });
+    
+            recentsListElement.appendChild(button);
         });
-
-        recentsButton.querySelector("div").appendChild(button);
     });
+
 });
