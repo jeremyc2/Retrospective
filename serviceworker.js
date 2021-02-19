3c0bb27b7d5a10751269d2d095e8587db8aec02e
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2021-02-19 13:23:33 -0600

    No commit message

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,19 @@
 const path = (new URL(self.registration.scope)).pathname;
 
-console.log("path", path);
-
 var cacheName = "Retro-V1.0";
 const cachefiles = [
     path,
-    path + "manifest.webmanifest"
+    path + "manifest.webmanifest",
+    path + "css/main.css",
+    path + "css/recents.css",
+    path + "css/tooltip.css",
+    path + "css/card.css",
+    path + "css/details.css",
+    path + "css/scrollbars.css",
+    path + "css/slider.css",
+    path + "manifest.webmanifest",
+    path + "img/icons/192.png",
+    path + "img/logo.svg"
 ]
 
 self.addEventListener("install", event => {
